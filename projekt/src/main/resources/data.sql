e3cee25fdbb2d53e7034442cccdf79ce3ffb4bdd
hajpson <kacpah@hotmail.com>
2020-04-18 09:32:50 +0200

    update

--- projekt/src/main/resources/data.sql
+++ projekt/src/main/resources/data.sql
@@ -1,11 +1,26 @@
 create database IF NOT EXISTS baza_na_zadanie;
 use baza_na_zadanie;
-create table IF NOT EXISTS Persona (
-    ID int NOT NULL,
-    Ulica varchar(30),
-    Nr_domu int NOT NULL,
-    Nr_mieszkania int,
-    Miasto varchar(30) NOT NULL,
-    Kod_pocztowy varchar(6) NOT NULL,
-    primary key (ID)
+
+create table IF NOT EXISTS Zamowienia (
+    ID_zamowienia int primary key,
+    Numer_zamowienia int NOT NULL,
+    Data_zamowienia date NOT NULL
 );
+create table IF NOT EXISTS Artykuly (
+    ID_artykulu int primary key,
+    Numer_artykulu int NOT NULL,
+    Cena_artykulu int NOT NULL
+);
+create table IF NOT EXISTS Artykuly_w_zamowieniu (
+    ID int primary key,
+    Numer_zamowienia int,
+    Numer_artykulu int,
+    Ilosc int NOT NULL,
+    Cena int NOT NULL
+);
+ALTER TABLE Artykuly_w_zamowieniu drop constraint fk_ID_zamowienia;
+ALTER TABLE Artykuly_w_zamowieniu drop constraint fk_ID_artykulu;
+ALTER TABLE Artykuly_w_zamowieniu add constraint fk_ID_zamowienia foreign key
+(Numer_zamowienia) references Zamowienia (ID_zamowienia);
+ALTER TABLE Artykuly_w_zamowieniu add constraint fk_ID_artykulu foreign key
+(Numer_artykulu) references Artykuly (ID_artykulu);
